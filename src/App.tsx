0a6e3d437b0f17eac7e6aa5a992d5d3da6cb85d6
Aiko Hoshi <aihokoshi@gmail.com>
2022-12-03 18:35:24 +0200

    Adding idea to ignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+type MessageType = {
+    message: string
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [message, setMessage] = useState<Array<MessageType>>([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
+    ])
+
+    return (
+        <div className="App">
+            <div>
+                <input type="text"/>
+                <button>+</button>
+            </div>
+            {message.map((el, index) => {
+                return <div key={index}>{el.message}</div>
+            })}
+        </div>
+    );
 }
 
 export default App;
